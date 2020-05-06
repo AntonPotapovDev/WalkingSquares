89ed25598b9c7d33b85fe5af2d2cef80ea418375
Anton Potapov <protestandprotect52@gmail.com>
2020-05-06 19:08:30 +0300

    Added game object class and test script

---
+++ public/js/game.object.js
@@ -0,0 +1,52 @@
+export class GameObject {
+	constructor() {
+		this.mesh = null;
+		this.fpsFactor = 1;
+	}
+	
+	addToScene(scene) {
+		scene.add(this.mesh);
+	}
+	
+	position() {
+		return this.mesh == null ? null : this.mesh.position;
+	}
+}
+
+export class MovableObject extends GameObject {
+	constructor() {
+		super();
+		this.speed = 0;
+		this._look_dir = new THREE.Vector3(0, 1, 0);
+	}
+	
+	move(x, y) {
+		this.mesh.position.x += this.fpsFactor * this.speed * x;
+		this.mesh.position.y += this.fpsFactor * this.speed * y;
+	}
+	
+	moveAlongLookDir() {
+		this.move(this._look_dir.x, this._look_dir.y);
+	}
+	
+	rotate(angle) {
+		this.mesh.rotation.z += angle;
+		this._look_dir.applyAxisAngle(angle);
+	}
+	
+	lookAt(x, y) {
+		let target = new THREE.Vector3(x, y, 0);
+		let dir = target.clone().sub(this.position().clone()).normalize();
+		let angle = dir.angleTo(this._look_dir);
+		let left = new THREE.Vector3(-this._look_dir.y, this._look_dir.x, 0);
+		let factor = target.sub(left).angleTo(this._look_dir) <= Math.PI / 2 ? 1 : -1;
+		this.rotate(factor * angle);
+	}
+}
+
+export class Unit extends MovableObject {
+	constructor() {
+		super();
+		this.hp = 0;
+	}
+}
---
+++ public/js/test.js
@@ -0,0 +1,24 @@
+import * as GObject from '/js/game.object.js';
+
+let scene = new THREE.Scene();
+let camera = new THREE.OrthographicCamera(innerWidth / - 2, innerWidth / 2, innerHeight / 2, innerHeight / - 2, 1, 1000);
+camera.position.z = 1;
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+let geometry = new THREE.PlaneGeometry(50, 50);
+let material = new THREE.MeshBasicMaterial({ color: 0x00ff00, side: THREE.DoubleSide });
+let square = new THREE.Mesh(geometry, material);
+
+let player = new GObject.Unit();
+player.mesh = square;
+player.addToScene(scene);
+player.lookAt(300, -150);
+
+function render() {	
+	requestAnimationFrame(render);     
+	renderer.render(scene, camera);
+}
+
+render();
