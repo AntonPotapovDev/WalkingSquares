18b7235c70fc23b4ee2aff603f59f64673c22e40
Anton Potapov <protestandprotect52@gmail.com>
2020-05-09 17:25:37 +0300

    Added class Control + new constructor in Game class

--- public/js/main.js
+++ public/js/main.js
@@ -148,8 +148,13 @@ class Shotgun extends Weapon {
 }
 
 class Game {
-	constructor() {
+	constructor(gameScene, control) {
 		this._fpsFactor = 1;
+		this._gameScene = gameScene;
+		this._control = control;
+		this._player = new Player();
+		this._enemies = [];
+		this._items = [];
 	}
 	
 	start() {
@@ -196,6 +201,66 @@ class GameScene {
 				i--;
 			}
 		}
+	}
+}
+
+class Control {
+	constructor(zoneWidth, zoneHeight) {
+		this._width = zoneWidth;
+		this._height = zoneHeight;
+		this._mouse = new THREE.Vector3(0, 0, 0);
+		this._moveVector = new THREE.Vector3(0, 0, 0);
+		this._keyMap = {};
+		this._isMouseDown = false;
+		this._mouseClickCount = 0;
+	}
+	
+	mouse() {
+		return this._mouse.clone();
+	}
+	
+	moveVector() {
+		return this._moveVector.clone();
+	}
+	
+	mouseClicks() {
+		return this._mouseClickCount;
+	}
+	
+	update() {
+		this._moveVector = new THREE.Vector3(0, 0, 0);
+		for (let key in this._keyMap) {
+			let value = this._keyMap[key];
+			
+			if (!value)
+				continue;
+
+			if (key == "KeyW") this._moveVector.y = 1;
+			if (key == "KeyS") this._moveVector.y = -1;
+			if (key == "KeyA") this._moveVector.x = -1;
+			if (key == "KeyD") this._moveVector.x = 1;
+		}
+	}
+	
+	onKeyDown(event) {
+		this._keyMap[event.code] = event.type == "keydown";
+	}
+	
+	onMouseMove(event) {
+		this._mouse.x = event.clientX - this._width / 2;
+		this._mouse.y = -event.clientY + this._height / 2;
+	}
+	
+	onMouseDown(event) {
+		this._isMouseDown = event.type == "mousedown";
+	}
+	
+	onMouseClick(event) {
+		this._mouseClickCount++;
+	}
+	
+	mouseClicksHandled() {
+		this._mouseClickCount = 0;
 	}
 }
 
