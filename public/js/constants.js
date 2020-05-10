9f13998a0e06a42e7c38431f1eaf07bd110f2a33
Anton Potapov <protestandprotect52@gmail.com>
2020-05-10 21:30:22 +0300

    Added constatnts

---
+++ public/js/constants.js
@@ -0,0 +1,25 @@
+export class PhisicalValues {
+	static playerSpeed     = 10;
+	static deadPlayerSpeed = 3;
+	
+	static enemyBaseSpeed   = 2;
+	static enemySpeedFactor = 3;
+	
+	static bulletSpeed = 20;
+}
+
+export class HpValues {
+	static playerHp = 100;
+	static enemyHP  = 1;
+}
+
+export class DamageValues {
+	static enemyDamage = 1;
+}
+
+export class TimeValues {
+	static baseEnemySpawnRate = 1000;
+	static enemySpawnDecrease = 2;
+	
+	static nextWeaponSpawnTimeout = 2 * 60 * 1000;
+}
