cfa8aedcbedd5984317445543d39c08bd25f3ed0
Anton Potapov <protestandprotect52@gmail.com>
2020-05-12 17:29:50 +0300

    Move files

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const port = 80;
 const express = require('express');
 const app = express();
 app.use(require('body-parser').json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + ''));
 
 app.listen(port);
