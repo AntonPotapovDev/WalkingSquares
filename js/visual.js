86014947c4f38a5d0f55621d7dac723f84aaccf5
Anton Potapov <protestandprotect52@gmail.com>
2020-05-16 00:03:15 +0300

    Change render order to implement z-indexing

--- js/visual.js
+++ js/visual.js
@@ -12,15 +12,17 @@ export class Colors {
 	}
 }
 
-function makeMesh(width, height, color) {
-	let geometry = new THREE.PlaneGeometry(width, height);
-	let material = new THREE.MeshBasicMaterial({ color: color, side: THREE.DoubleSide });
-	return new THREE.Mesh(geometry, material);
+function makeMesh(width, height, color, zIndex = 0) {
+	let mesh = new THREE.Mesh(
+		new THREE.PlaneGeometry(width, height),
+		new THREE.MeshBasicMaterial({ color: color, side: THREE.DoubleSide}));
+	mesh.renderOrder = zIndex;
+	return mesh;
 }
 
 export class Meshes {
-	static playerMesh() { return makeMesh(50, 50, Colors.playerColor); }
-	static enemyMesh()  { return makeMesh(50, 50, Colors.enemyColor); }
+	static playerMesh() { return makeMesh(50, 50, Colors.playerColor, 1); }
+	static enemyMesh()  { return makeMesh(50, 50, Colors.enemyColor, 2); }
 	static bulletMesh() { return makeMesh(5, 10, Colors.bulletColor); }
 	static weaponBoxMesh() { return makeMesh(20, 20, Colors.weaponBoxColor); }
 	static medkitMesh() { return makeMesh(20, 20, Colors.medkitColor); }
