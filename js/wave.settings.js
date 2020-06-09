f55f7cd7803280fb5f52071b8bc2030e58be06ce
Anton Potapov <protestandprotect52@gmail.com>
2020-06-09 18:27:16 +0300

    Update waves settings

--- js/wave.settings.js
+++ js/wave.settings.js
@@ -5,8 +5,85 @@ let waveSettings = [
 		enemyCount:   10,
 		startTimeout: 5,
 		spawnRate:    1,
-		maxItems:     5,
+		maxItems:     0,
+		weapons:      []
+	},
+	{
+		enemyCount:   20,
+		startTimeout: 5,
+		spawnRate:    0.9,
+		maxItems:     1,
+		weapons:      []
+	},
+	{
+		enemyCount:   30,
+		startTimeout: 5,
+		spawnRate:    0.8,
+		maxItems:     2,
+		weapons:      []
+	},
+	{
+		enemyCount:   50,
+		startTimeout: 5,
+		spawnRate:    0.7,
+		maxItems:     3,
+		weapons:      []
+	},
+	{
+		enemyCount:   70,
+		startTimeout: 5,
+		spawnRate:    0.6,
+		maxItems:     3,
+		weapons:      [ ]
+	},
+	{
+		enemyCount:   100,
+		startTimeout: 5,
+		spawnRate:    0.5,
+		maxItems:     3,
 		weapons:      [ new Weapon.Shotgun() ]
+	},
+	{
+		enemyCount:   150,
+		startTimeout: 5,
+		spawnRate:    0.4,
+		maxItems:     3,
+		weapons:      []
+	},
+	{
+		enemyCount:   200,
+		startTimeout: 5,
+		spawnRate:    0.3,
+		maxItems:     5,
+		weapons:      [ new Weapon.SubmachineGun() ]
+	},
+	{
+		enemyCount:   300,
+		startTimeout: 5,
+		spawnRate:    0.25,
+		maxItems:     10,
+		weapons:      []
+	},
+	{
+		enemyCount:   500,
+		startTimeout: 5,
+		spawnRate:    0.2,
+		maxItems:     10,
+		weapons:      [ new Weapon.Minigun() ]
+	},
+	{
+		enemyCount:   700,
+		startTimeout: 5,
+		spawnRate:    0.15,
+		maxItems:     10,
+		weapons:      []
+	},
+	{
+		enemyCount:   1000,
+		startTimeout: 5,
+		spawnRate:    0.1,
+		maxItems:     15,
+		weapons:      []
 	}
 ];
 
